5ffcc3f0b2af8c86093cec03578bd8adaeaf895d
eljefe <jeff.clairmont@gmail.com>
2015-11-19 23:55:25 -0500

    redo comment controller and redesign views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,22 @@
 class CommentsController < ApplicationController
   
-  before_action :find_comment, only: [:show, :edit, :update, :destroy]
+  before_action :find_post
+  before_action :find_comment, only: [:edit,:update,:destroy]
   
   def show
   end
-  
+
   def new
     @comment = Comment.new
+    @comment.post_id = @post.id
+    @comment.user_id = current_user.id
   end
+  
   
   def create 
     @comment = Comment.new(comment_params)
     if @commnent.save
-      redirect_to root_path
+      redirect_to post_path(@post)
     else
       render 'new'
     end
@@ -22,9 +26,15 @@ class CommentsController < ApplicationController
   end 
   
   def update
+    If @comment.update(comment_params)
+    redirect_to post_path(@post)
+  else
+    render 'edit'
   end
   
   def destroy
+    @comment.destroy
+    redirect_to root_path
   end
   
   private
@@ -33,7 +43,11 @@ class CommentsController < ApplicationController
     params.require(:comment).perimit(:body)
   end
   
-  def find_comment
-    @comment = Comment.find(params[:id])
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
   
+  def find_comment
+    @comment = Comment.find(params[:book_id])
+  end
 end
